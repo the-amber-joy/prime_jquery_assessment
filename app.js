ad5a548a0ecd6468a91213a07a2115f82d777677
Amber Joy <amber.joy@gmail.com>
2015-12-16 15:01:04 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var clickCount = 0
+
+$(document).ready(function() {
+	$('body').on('click', '.generate', buildContainer);
+	$('body').on('click', '.generate', countClicks);
+	$('body').on('click', '.change', changeColor);
+	$('body').on('click', '.delete', deleteRow);
+});
+
+
+function buildContainer(){
+	$el = $(this).parent()
+	$el.append('<div class="container">' + 
+		'<button class="delete">delete</button><br><p>' + 
+		clickCount + 
+		'<br><button class="change">change</change>' + 
+		'</div>');
+}
+
+
+function countClicks(){
+	clickCount++
+}
+
+
+function changeColor(){
+	$(this).parent().parent().toggleClass('red');
+}
+
+
+function deleteRow(){
+	$(this).parent().remove();
+}
+
